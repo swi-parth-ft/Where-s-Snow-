94b07a0c4cf91a422a738a42725f06248c67b652
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-23 14:08:00 -0400

    create resortView

---
+++ Where's Snow?/ResortView.swift
@@ -0,0 +1,103 @@
+//
+//  ResortView.swift
+//  Where's Snow?
+//
+//  Created by Parth Antala on 2024-07-23.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    let resort: Resort
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    ResortDetailView(resort: resort)
+                    SkiDetailView(resort: resort)
+                }
+                .padding(.vertical)
+                .background(.primary.opacity(0.1))
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Fecilities")
+                        .font(.headline)
+                    
+                    Text(resort.facilities, format: .list(type: .and))
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationTitle("\(resort.name), \(resort.country)")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct SkiDetailView: View {
+    let resort: Resort
+    var body: some View {
+        Group {
+            VStack {
+                Text("Elevation")
+                    .font(.caption.bold())
+                Text("\(resort.elevation)m")
+                    .font(.title3)
+                
+            }
+            
+            VStack {
+                Text("Snow")
+                    .font(.caption.bold())
+                Text("\(resort.snowDepth)cm")
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct ResortDetailView: View {
+    let resort: Resort
+    var size: String {
+        switch resort.size {
+        case 1: "small"
+        case 2: "Average"
+        default: "Large"
+        }
+    }
+    
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("Size")
+                    .font(.caption.bold())
+                Text(size)
+                    .font(.title3)
+                
+            }
+            
+            VStack {
+                Text("Price")
+                    .font(.caption.bold())
+                Text(price)
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+#Preview {
+    ResortView(resort: .example)
+}
