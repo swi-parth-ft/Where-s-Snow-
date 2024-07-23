f5488d0db5d86a9ff66c39e7516b75e6b44751dc
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-23 10:41:15 -0400

    Initial Commit

---
+++ Where's Snow?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Where's Snow?
+//
+//  Created by Parth Antala on 2024-07-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Where's Snow?/Where_s_Snow_App.swift
@@ -0,0 +1,17 @@
+//
+//  Where_s_Snow_App.swift
+//  Where's Snow?
+//
+//  Created by Parth Antala on 2024-07-23.
+//
+
+import SwiftUI
+
+@main
+struct Where_s_Snow_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
